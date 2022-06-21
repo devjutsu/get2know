148b2aa3a5e828c7ea1afc078686fa4d90d01555
Albert Garuda <algarud@gmail.com>
2022-06-21 17:21:57 +0300

    twitter data retrieval

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { Client } from "twitter-api-sdk";
 
 type Data = {
   name: string
 }
 
-export default function handler(
+const client = new Client(process.env.BEARER_TOKEN || '');
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+
+  // const response = await fetch('http://twitter.com');
+  // const data = await response.text();
+  // console.log(data);
+
+
+  const user = await client.users.findUserByUsername('devjutsu');
+  // console.log('user:', user.data);
+  
+  const tweet = (await client.tweets.findTweetById('1500526320094171145'));
+  console.log(tweet);
+  // const tmp = twee tweet.data?.text || tweet.data?.source;
+  
+  res.status(200).json({ name: tweet.data?.text || '-'});
 }
